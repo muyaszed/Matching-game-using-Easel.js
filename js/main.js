b6f2cd829e31325f5b09636ad9be5d0132b95351
Yazed Jamal <muyaszed@gmail.com>
2017-10-13 09:56:53 +0100

    setup multiple square rendering base on rows and columns

--- js/main.js
+++ js/main.js
@@ -1,23 +1,40 @@
+var squarHeight = 200;
+var squareWidth = 200;
+var squareGap = 10;
+var column = 3;
+var row = 2;
+
 function init() {
-	let stage = new createjs.Stage("myCanvas");
-	let square = drawSquare();
+	var stage = new createjs.Stage("myCanvas");
+	var square;
 	
 
-	stage.addChild(square);
-	stage.update();
+	for(i=0; i < column*row; i++) {
+		
+		square = drawSquare();
+		square.x = (squareWidth+squareGap)*(i%column);
+		square.y = (squarHeight+squareGap)*Math.floor(i/column);	
+		stage.addChild(square);
+		stage.update();
+		// x += 80;
+	}
+	
+	
+
+	
 }
 
 function drawSquare() {
-	let graphics = new createjs.Graphics().setStrokeStyle(5).beginStroke("rgba(20,20,20,1)")
-	graphics.beginFill(randomColor()).rect(5,5,70,70);
-	let shape = new createjs.Shape(graphics);
+	var graphics = new createjs.Graphics().setStrokeStyle(5).beginStroke("rgba(20,20,20,1)")
+	graphics.beginFill(randomColor()).rect(5,5,squarHeight,squareWidth);
+	var shape = new createjs.Shape(graphics);
 	return shape;
 }
 
 function randomColor() {
-	let num1 = Math.floor(Math.random()*255);
-	let num2 = Math.floor(Math.random()*255);
-	let num3 = Math.floor(Math.random()*255);
+	var num1 = Math.floor(Math.random()*255);
+	var num2 = Math.floor(Math.random()*255);
+	var num3 = Math.floor(Math.random()*255);
 	return "rgba("+num1+","+num2+","+num3+",1)";
 	
 }
